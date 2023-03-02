8e82fd33f52a48b103daed10e4accd37605e0e55
Roman Dvořák <romandvorak@mlab.cz>
2023-03-02 18:52:04 +0100

    add github actions

---
+++ fw/simple_platform/githash.h
@@ -0,0 +1,5 @@
+// This file is overwritten by github actions, do not update it manually
+String githash = "c92661acdd51ac8095a079ab44ea089d5385ede0,User";
+#define GHRELEASE 0
+#define GHBUILD 0
+#define GHBUILDTYPE User
